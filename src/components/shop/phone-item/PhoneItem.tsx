aa90e111d8b9c6b233f287db218e3710d133522d
grvpande <grvpande@gmail.com>
2022-09-04 20:47:16 +0530

    changes|

--- src/components/shop/phone-item/PhoneItem.tsx
+++ src/components/shop/phone-item/PhoneItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './styles.module.scss';
 
 export default function PhoneItem() {
     return <div>PhoneItem</div>;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,13 @@
+import PhoneItem from 'components/shop/phone-item/PhoneItem';
 import Container from 'components/ui/layout/Container';
 import { homePageDescription, homePageTitle } from 'config';
 import useSeo from 'hooks/useSeo';
 
 export default function Home() {
     useSeo({ title: homePageTitle, description: homePageDescription });
-    return <Container>Home</Container>;
+    return (
+        <Container>
+            <PhoneItem />
+        </Container>
+    );
 }
